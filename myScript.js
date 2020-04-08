22a0b3052255356bbb468d436e7b8b534c06bed1
cavalerii <63225438+cavalerii@users.noreply.github.com>
2020-04-08 14:21:48 +0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,11 +1,3 @@
 function translatePage()  {
 	document.getElementById("titlu").innerHTML = "Film recommendations";
-	document.getElementById("titlu_filme").innerHTML = "Movies";
-	document.getElementById("titlu_seriale").innerHTML = "Series";
-}
-
-function schimbaCulori() {
-	document.getElementById("titlu").style.color = "tomato";
-	document.getElementById("titlu_filme").style.color = "tomato";
-	document.getElementById("titlu_seriale").style.color = "tomato";
 }
