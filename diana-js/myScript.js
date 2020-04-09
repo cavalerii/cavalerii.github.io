45bcd41206797622ae493c2ba65fe7aa85344cac
cavalerii <63225438+cavalerii@users.noreply.github.com>
2020-04-09 14:39:57 +0300

    Add files via upload

---
+++ diana-js/myScript.js
@@ -0,0 +1,26 @@
+var nume = ""
+var age = ""
+var color = "
+
+}
+
+function askForName() {
+	nume = prompt("Introduceti numele:")
+	document.getElementById("nume_user").innerHTML = "Salut, " + nume + "!"
+}
+
+function askForAge() {
+	 age = prompt("Introduceti varsta")
+	document.getElementById("varsta_user").innerHTML = age + " ani"
+}
+
+function askForColor() {
+	 color = prompt("Introdu culoarea preferata")
+	document.getElementById("culoare").innerHTML = "Culoare preferata: " + color
+}
+
+function resumeUser() {
+	document.getElementById("rezumat").innerHTML = "Esti " + nume + ". Ai " + age + " ani. Culoarea ta preferata este " + color + " ."
+}
+
+
