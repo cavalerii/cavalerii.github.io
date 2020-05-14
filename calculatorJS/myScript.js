9d683414df81b5f3ac36d479199748ce660b00a8
IoanaPetrea <ioana.petrea@turnulinclinat.ro>
2020-05-14 14:55:40 +0300

    display result in html

--- calculatorJS/myScript.js
+++ calculatorJS/myScript.js
@@ -3,14 +3,16 @@ let secondNumber=0
 
 function askForFirstNumber(){
  let text = prompt("Introdu primul numar") 
-firstNumber=parseInt(text,10)
+document.getElementById("firstNumber").innerHTML= `Primul număr: ` + text; 
+ firstNumber=parseInt(text,10)
 }
 
 function askForSecondNumber(){
 let text = prompt("Introdu al doilea numar")
+document.getElementById("secondNumber").innerHTML= `Al doile număr: ` + text; 
 secondNumber=parseInt(text,10)
 }
 function displayResult(){
  let result= firstNumber+ secondNumber
-        confirm(`Rezultatul este ${result}`)
+document.getElementById("resume").innerHTML= `Rezultatul este ${result}`  
 }
