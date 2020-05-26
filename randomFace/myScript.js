61b920c78cc44ac0692da7c89f087b2728e3439d
Larisa20 <larilarisa033@gmail.com>
2020-05-26 15:05:41 +0300

    add images for group 2

--- randomFace/myScript.js
+++ randomFace/myScript.js
@@ -3,3 +3,8 @@ function selectRandom() {
     let imageName="gr1_" + random + ".jpg";
     document.getElementById("pozaRandom").src=imageName;
 }
+function selectRandom2() {
+    let random=Math.floor(Math.random()*6+1);
+    let imageName="gr2_" + random + ".jpg";
+    document.getElementById("pozaRandom2").src=imageName;
+}
