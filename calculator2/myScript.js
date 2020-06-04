d827ee977a72e7327d9230a2bd955fe04bde33cc
sotina55 <64959716+sotina55@users.noreply.github.com>
2020-06-04 13:46:45 +0300

    Update myScript.js

--- calculator2/myScript.js
+++ calculator2/myScript.js
@@ -9,4 +9,24 @@ function button9() {
 }
 function buttonDivide() {
   document.getElementById("calcul").value="/";
+
+}
+function button6() {
+  document.getElementById("calcul").value="6";
+
+}
+function button5() {
+  document.getElementById("calcul").value="5";
+
+}
+function button4() {
+  document.getElementById("calcul").value="4";
+
+}
+function button3() {
+  document.getElementById("calcul").value="3";
+
+}
+function buttonMultiply() {
+  document.getElementById("calcul").value="×";
 }
