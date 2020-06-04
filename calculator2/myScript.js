e5da1b21e9dbfbe50cf7bf7b879d471f5e6c98f2
axandrei <60612181+axandrei@users.noreply.github.com>
2020-06-04 13:34:54 +0300

    Update myScript.js

--- calculator2/myScript.js
+++ calculator2/myScript.js
@@ -1,3 +1,12 @@
 function button7() {
   document.getElementById("calcul").value="7";
+
+function button8() {
+  document.getElementById("calcul").value="8";
+}
+function button9() {
+  document.getElementById("calcul").value="9";
+}
+function buttonDivide() {
+  document.getElementById("calcul").value="/";
 }
