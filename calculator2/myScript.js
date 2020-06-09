532e588219a913b72224e8cfedcda70f23938a25
Mihai Pantiru <mihai.pantiru@gmail.com>
2020-06-09 12:59:44 +0300

    Revert "Merge branch 'master' of https://github.com/cavalerii/cavalerii.github.io"
    This reverts commit 0c1586f7d07e0872df8c1bd5351d3dd8edff6579.

---
+++ calculator2/myScript.js
@@ -0,0 +1,59 @@
+function button7() {
+  document.getElementById("calcul").value="7";
+}
+function button8() {
+  document.getElementById("calcul").value="8";
+}
+function button9() {
+  document.getElementById("calcul").value="9";
+}
+function buttonDivide() {
+  document.getElementById("calcul").value="/";
+
+}
+function button6() {
+  document.getElementById("calcul").value="6";
+
+}
+function button5() {
+  document.getElementById("calcul").value="5";
+
+}
+function button4() {
+  document.getElementById("calcul").value="4";
+
+}
+function button3() {
+  document.getElementById("calcul").value="3";
+
+}
+
+function button0() {
+  document.getElementById("calcul").value="0";
+
+}
+
+function buttonPlus() {
+  document.getElementById("calcul").value="+";
+
+}
+function buttonMultiply() {
+  document.getElementById("calcul").value="×";
+}
+
+function button2() {
+  document.getElementById("calcul").value="2";
+}
+
+function button1() {
+  document.getElementById("calcul").value="1";
+}
+
+function buttonMinus() {
+  document.getElementById("calcul").value="-";
+}
+
+function buttonPunct() {
+  document.getElementById("calcul").value=".";
+
+}
