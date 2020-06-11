077f527224e964c7fa1d92c7babb80e2bbd95864
nicoleta2003 <65164559+nicoleta2003@users.noreply.github.com>
2020-06-11 13:18:01 +0300

    Update bigEval.js

--- calculator2/bigEval.js
+++ calculator2/bigEval.js
@@ -1,3 +1,90 @@
+
+
+let input="" ;
+
+
+function button7() {
+  input=input+"7";
+  document.getElementById("calcul").value=input;
+
+}
+function button8() {
+input = input + "8" ;
+  document.getElementById("calcul").value=input;
+}
+function button9() {
+input = input + "9" ;
+  document.getElementById("calcul").value=input;
+}
+function buttonDivide() {
+input = input + "/" ;
+  document.getElementById("calcul").value=input;
+}
+function button6() {
+input = input + "6" ;
+  document.getElementById("calcul").value=input;
+
+}
+function button5() {
+input = input + "5" ;
+  document.getElementById("calcul").value=input;
+
+}
+function button4() {
+input = input + "4" ;
+  document.getElementById("calcul").value=input;
+
+}
+function button3() {
+input = input + "3" ;
+  document.getElementById("calcul").value=input;
+
+}
+
+function button0() {
+input = input + "0" ;
+  document.getElementById("calcul").value=input;
+
+}
+
+function buttonPlus() {
+input = input + "+" ;
+  document.getElementById("calcul").value=input;
+
+}
+function buttonMultiply() {
+input = input + "*" ;
+  document.getElementById("calcul").value=input;
+}
+
+function button2() {
+input = input + "2" ;
+  document.getElementById("calcul").value=input;
+}
+
+function button1() {
+input = input + "1" ;
+  document.getElementById("calcul").value=input;
+}
+
+function buttonMinus() {
+input = input + "-" ;
+  document.getElementById("calcul").value=input;
+}
+
+function buttonPunct() {
+input = input + "." ;
+  document.getElementById("calcul").value=input;
+
+}
+function buttonreset() {
+input = "";
+document.getElementById("calcul").value = input;
+} 
+
+function equal() {
+  document.getElementById("calcul").value = "";
+}
 /*
 	BigEval.js
 	by Avi Aryan (http://aviaryan.in)
