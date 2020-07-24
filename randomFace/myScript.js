ae3cc8596f8331168fb55a972a1b1c757e6c27c7
Mihai Pantiru <mihai.pantiru@gmail.com>
2020-07-24 13:27:32 +0300

    Update myScript.js

--- randomFace/myScript.js
+++ randomFace/myScript.js
@@ -3,8 +3,15 @@ function selectRandom() {
     let imageName="gr1_" + random + ".jpg";
     document.getElementById("pozaRandom").src=imageName;
 }
+
 function selectRandom2() {
     let random=Math.floor(Math.random()*7+1);
     let imageName="gr2_" + random + ".jpg";
     document.getElementById("pozaRandom2").src=imageName;
+}
+
+function selectRandomP() {
+    let random=Math.floor(Math.random()*8+1);
+    let imageName="grp_" + random + ".jpg";
+    document.getElementById("pozaRandomP").src=imageName;
 }
